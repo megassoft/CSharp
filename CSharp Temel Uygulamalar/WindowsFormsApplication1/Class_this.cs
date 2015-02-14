eaf1ebaca5a1c371784ad0cd1cc429352a323227
megassoft <megassoft@hotmail.com>
2015-02-14 18:23:44 +0200

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	CSharp Temel Uygulamalar/WindowsFormsApplication1/Class_this.cs

--- CSharp Temel Uygulamalar/WindowsFormsApplication1/Class_this.cs
+++ CSharp Temel Uygulamalar/WindowsFormsApplication1/Class_this.cs
@@ -59,7 +59,7 @@ namespace WindowsFormsApplication1
         public void yeni5(int weight, string name)
         {
 
-            Console.WriteLine("yeni51");
+            Console.WriteLine("yeni53");
 
 
         }
