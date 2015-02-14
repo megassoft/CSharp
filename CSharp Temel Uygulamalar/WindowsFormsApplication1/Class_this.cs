e0bdb53806362d6cc1d74a9ce17e3c2e176b1571
megassoft <megassoft@hotmail.com>
2015-02-14 10:40:03 +0200

    ok

---
+++ CSharp Temel Uygulamalar/WindowsFormsApplication1/Class_this.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsFormsApplication1
+{
+    class Class_this
+    {
+
+        //overloaded constructor initializer
+        //default initialize
+        //diyelim ki new ile instance yaratırken parametresiz tanımlarsak o zaman default olarak this ifadesi ile belirlediğimiz parametreleri overloaded olan constructor a aktaracaktır
+        //bir başka avantajı new ile çağırıldığında diğer constructor ların da çalışması için çağırılan yerde satır yazmak yerine bir kere yazılıp direkt this ile yönlendirme yapılmış olur
+        
+        public Class_this() : this(-1, "noname")
+        {
+
+            Console.WriteLine("default constructor");
+
+        }
+
+        public Class_this(int weight) : this (weight, "temp")
+        {
+
+            Console.WriteLine("Constructor1 weight = {0}", weight);
+
+        }
+
+        public Class_this(int weight, string name)
+        {
+
+            Console.WriteLine("Constructor2 weight = {0}, name = {1}", weight, name);
+
+        }
+
+    }
+
+    class Net
+    {
+        public string Name { get; set; }
+
+        public Net(Perl perl_sinifi)
+        {
+            // Use name from Perl instance.
+            // this kullanılmasa da olur. Çünkü parametre adı ile lokal değişken adı aynı olmadığından gerekli değildir
+            this.Name = perl_sinifi.Name;
+        }
+    }
+
+
+    class Perl
+    {
+        public string Name { get; set; }
+
+        public Perl(string name)
+        {
+            // this kullanılmasa da olur. Çünkü parametre adı ile lokal değişken adı aynı olmadığından gerekli değildir
+            this.Name = name;
+
+            // Pass this instance as a parameter!
+            Net net = new Net(this);
+
+            // The Net instance now has the same name.
+            Console.WriteLine(net.Name);
+        }
+    }
+
+}
