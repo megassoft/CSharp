0a638988ce07f9d97b465d1c1ef68bd9df67b956
megassoft <megassoft@hotmail.com>
2015-02-28 17:03:49 +0200

    .

--- CSharp Temel Uygulamalar/WindowsFormsApplication1/Class_Indexer.cs
+++ CSharp Temel Uygulamalar/WindowsFormsApplication1/Class_Indexer.cs
@@ -8,6 +8,50 @@ namespace WindowsFormsApplication1
     class Class_Indexer
     {
 
+        int[] indeks;
+
+        public int lenght;
+        public bool error;
+
+        public Class_Indexer(int size)
+        {
+            indeks = new int[size];
+            lenght = size;
+        }
+
+        public int this[int index]
+        {
+            get {
+                
+                if (index >= 0 & index < lenght)
+                {
+                    error = false;
+                    return indeks[index];
+                }
+                else
+                {
+                    error = true;
+                    return 0;
+                }
+
+            }
+
+            set { 
+            
+                if (index >= 0 & index < lenght)
+                {
+
+                    indeks[index] = value;
+                    error = false;
+                }
+                else
+                {
+                    error = true;
+                }
+
+            }
+        }
+
 
 
     }
